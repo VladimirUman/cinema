bb3747b02e3cd847aa5bc672fc957e1843bbfedb
RadetskiyBogdan <bogdan20226@gmail.com>
2022-02-09 11:40:47 +0200

    fix user account

--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -1,8 +1,13 @@
-import React, { useEffect, useState, useCallback } from 'react'
+import React, { useEffect, useState, useCallback,Component } from 'react'
 
 import api from '../api'
 
 import styled from 'styled-components'
+
+const Update = styled.div`
+    color: #ef9b0f;
+    cursor: pointer;
+`
 
 const Wrapper = styled.div`
     padding: 0 40px 40px 40px;
@@ -11,25 +16,41 @@ const Title = styled.h1.attrs({
     className: 'h1',
 })``
 
+class UpdateUser extends Component {
+    updateUser = event => {
+        event.preventDefault()
 
-function UsersList() {
+        window.location.href = `/users/update/${this.props.id}`
+    }
+
+    render() {
+        return <Update onClick={this.updateUser}>Update</Update>
+    }
+}
+
+
+function Account() {
      const [user, setUser] = useState({});
        
-     useEffect(() => {
-             fetchData();
-  },);
 
     const fetchData = useCallback(async () => {
         await api.getAccount().then(response => {
             setUser(response.data.user)
            })
        }, []);
+
+       useEffect(() => {
+                fetchData();
+       },[fetchData]);
+
      return (
             <Wrapper>
                 <Title>{user.name}</Title>
                 <Title>{user.lastName}</Title>
                 <Title>{user.email}</Title>
+
+                <span><UpdateUser id={user._id} /></span>
             </Wrapper>
      );
 }
-export default UsersList
+export default Account
